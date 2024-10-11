5566aa1f8d2bad98db0e638762059cf101bec9a6
Abhinav Vishwakarma <88868156+Abhinav-Vishwakarma@users.noreply.github.com>
2024-10-11 18:06:04 +0530

    Fix Menu Bar and redesigned the event cards

--- main/js/script.js
+++ main/js/script.js
@@ -1,20 +1,4 @@
-const card = document.getElementById('card');
-const message = document.getElementById('message');
-let showBack = false;
+function redirectToEvent(url) {
+    window.open(url, "_blank");
+}
 
-card.addEventListener('click', () => {
-    showBack = !showBack;
-    if (showBack) {
-        card.classList.add('show-back');
-    } else {
-        card.classList.remove('show-back');
-    }
-});
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) { 
-        message.style.display = 'block';
-    } else {
-        message.style.display = 'none';
-    }
-});
